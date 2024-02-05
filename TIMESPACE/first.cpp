e1b921fe1fd97f5d31329487184d3d715d85081b
Komal Joshi <moudgillkomal6@gmail.com>
2024-02-05 21:50:32 +0530

    modified:   TIMESPACE/first.cpp

--- TIMESPACE/first.cpp
+++ TIMESPACE/first.cpp
@@ -2,4 +2,4 @@
 
 // time complexity is a measure of how the running time of an algorithm grows as the size of the input increases
 
-// space complexity measures how
+// Space complexity measures how the memory/space required by an algorithm grows as the size of the input increases.
