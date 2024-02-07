dc2f12ef88130be427552662681eb14a49436fb1
Komal Joshi <moudgillkomal6@gmail.com>
2024-02-07 19:37:08 +0530

    new file:   Calculation_Time.cpp 	renamed:    fourth.cpp -> Types_Time.cpp

--- Types_Time.cpp
+++ Types_Time.cpp
@@ -1,6 +1,8 @@
+// COMMON TIME COMPLEXITIES
+
 // O(1): Constant time complexity
 // O(log⁡ n): Logarithmic time complexity
 // O(n): Linear time complexity
 // O(nlog⁡n): Linearithmic time complexity
-// O(n2): Quadratic time complexity
-// O(2n): Exponential time complexity
+// O(n^2): Quadratic time complexity
+// O(2^n): Exponential time complexity
