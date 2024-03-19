10337da3f1533bfd75e781054ba7eddb6b213e7b
Komal Joshi <moudgillkomal6@gmail.com>
2024-03-19 19:21:51 +0530

    modified:   custom_array.cpp

--- custom_array.cpp
+++ custom_array.cpp
@@ -1,73 +1,73 @@
-lass Main {
-    public static void main(String[] args) {
-        // Note: Don't change class name
-        // your code goes here
-        DynamicArray arr = new DynamicArray();
+// lass Main {
+//     public static void main(String[] args) {
+//         // Note: Don't change class name
+//         // your code goes here
+//         DynamicArray arr = new DynamicArray();
 
-        for(int i = 0; i < 45; ++i) {
-            System.out.println(arr.size() + " - " + arr.capacity());
-            arr.add(i);
-        }
-    }
-}
+//         for(int i = 0; i < 45; ++i) {
+//             System.out.println(arr.size() + " - " + arr.capacity());
+//             arr.add(i);
+//         }
+//     }
+// }
 
-class DynamicArray {
-    private int arr[];
-    private int capacity = 10;
-    private int size = 0;
+// class DynamicArray {
+//     private int arr[];
+//     private int capacity = 10;
+//     private int size = 0;
 
-    DynamicArray() {
-        arr = new int[10];
-        size = 0;
-        capacity = 10;
-    }
+//     DynamicArray() {
+//         arr = new int[10];
+//         size = 0;
+//         capacity = 10;
+//     }
 
-    void add(int num) {
-        // We've to double the capacity
-        if(size == capacity) {
-            int newArr[] = new int[2*capacity];
+//     void add(int num) {
+//         // We've to double the capacity
+//         if(size == capacity) {
+//             int newArr[] = new int[2*capacity];
 
-            // Copy the elements from the old array.
-            for(int i = 0; i < capacity; ++i)
-                newArr[i] = arr[i];
+//             // Copy the elements from the old array.
+//             for(int i = 0; i < capacity; ++i)
+//                 newArr[i] = arr[i];
 
-            // Now, we have to start using the
-            // new array for storing the elements.
-            this.arr = newArr;
+//             // Now, we have to start using the
+//             // new array for storing the elements.
+//             this.arr = newArr;
 
-            capacity *= 2;
-        }
+//             capacity *= 2;
+//         }
 
-        arr[size] = num;
-        size++;
-    }
+//         arr[size] = num;
+//         size++;
+//     }
 
-    void remove_back() {
-        if(size > 0) {
-            this.arr[size-1] = 0;
-            size--;
-        }
-    }
+//     void remove_back() {
+//         if(size > 0) {
+//             this.arr[size-1] = 0;
+//             size--;
+//         }
+//     }
 
-    void set(int id, int num) {
-        if(id >= 0 && id < size) {
-            this.arr[id] = num;
-        }
-    }
+//     void set(int id, int num) {
+//         if(id >= 0 && id < size) {
+//             this.arr[id] = num;
+//         }
+//     }
 
-    int get(int id) {
-        if(id >= 0 && id < size)
-            return this.arr[id];
+//     int get(int id) {
+//         if(id >= 0 && id < size)
+//             return this.arr[id];
 
-        return -1; // Ideally, an exception should be shown.
-    }
+//         return -1; // Ideally, an exception should be shown.
+//     }
 
 
-    int size() {
-        return this.size;
-    }
+//     int size() {
+//         return this.size;
+//     }
 
-    int capacity() {
-        return this.capacity;
-    }
-}
+//     int capacity() {
+//         return this.capacity;
+//     }
+// }
