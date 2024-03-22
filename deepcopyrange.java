a27ee2e76d476f2a957cf452b790658af68ae633
Komal Joshi <moudgillkomal6@gmail.com>
2024-03-22 13:50:10 +0530

    modified:   deepcopyrange.java

--- deepcopyrange.java
+++ deepcopyrange.java
@@ -44,22 +44,22 @@ public class Solution {
     // Now link the random pointers of the new nodes created.
     // Iterate the newly created list and use the original nodes' random pointers,
     // to assign references to random pointers for cloned nodes.
-    while (ptr != null) {
-      ptr.next.random = (ptr.random != null) ? ptr.random.next : null;
-      ptr = ptr.next.next;
-    }
+//     while (ptr != null) {
+//       ptr.next.random = (ptr.random != null) ? ptr.random.next : null;
+//       ptr = ptr.next.next;
+//     }
 
-    Unweave the linked list to get back the original linked list and the cloned list.
-    i.e. A->A'->B->B'->C->C' would be broken to A->B->C and A'->B'->C'
-    Node ptr_old_list = head; // A->B->C
-    Node ptr_new_list = head.next; // A'->B'->C'
-    Node head_new = head.next;
-    while (ptr_old_list != null) {
-      ptr_old_list.next = ptr_old_list.next.next;
-      ptr_new_list.next = (ptr_new_list.next != null) ? ptr_new_list.next.next : null;
-      ptr_old_list = ptr_old_list.next;
-      ptr_new_list = ptr_new_list.next;
-    }
-    return head_new;
-  }
-}
+//     Unweave the linked list to get back the original linked list and the cloned list.
+//     i.e. A->A'->B->B'->C->C' would be broken to A->B->C and A'->B'->C'
+//     Node ptr_old_list = head; // A->B->C
+//     Node ptr_new_list = head.next; // A'->B'->C'
+//     Node head_new = head.next;
+//     while (ptr_old_list != null) {
+//       ptr_old_list.next = ptr_old_list.next.next;
+//       ptr_new_list.next = (ptr_new_list.next != null) ? ptr_new_list.next.next : null;
+//       ptr_old_list = ptr_old_list.next;
+//       ptr_new_list = ptr_new_list.next;
+//     }
+//     return head_new;
+//   }
+// }
