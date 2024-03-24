414050acfc4b7179560e027eb304bdc2a4c60e2b
Komal Joshi <moudgillkomal6@gmail.com>
2024-03-24 10:05:44 +0530

    modified:   inbuilt_array.cpp

--- inbuilt_array.cpp
+++ inbuilt_array.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 
-int main() {
-    vector<int> v1;
-    vector<int> v2(5, 10);
-    vector<int> v3 = {5, 1, 3, 2};
+// int main() {
+//     vector<int> v1;
+//     vector<int> v2(5, 10);
+//     vector<int> v3 = {5, 1, 3, 2};
 
-    cout << v2.size() << '\n';
+//     cout << v2.size() << '\n';
 
-    for(int i = 0; i < v2.size(); ++i)
-        cout << v2[i] << ' ';
-    cout << '\n';
+//     for(int i = 0; i < v2.size(); ++i)
+//         cout << v2[i] << ' ';
+//     cout << '\n';
 
-    for(int i = 0; i < v3.size(); ++i)
-        cout << v3[i] << ' ';
-    cout << '\n';
+//     for(int i = 0; i < v3.size(); ++i)
+//         cout << v3[i] << ' ';
+//     cout << '\n';
 
 
-    v1.push_back(5);
-    v3.push_back(10);
-    v3.push_back(15);
+//     v1.push_back(5);
+//     v3.push_back(10);
+//     v3.push_back(15);
 
     for(int i = 0; i < v3.size(); ++i)
         cout << v3[i] << ' ';
