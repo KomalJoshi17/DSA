2ded547ecd5bd5a994693d73f137554105183c26
Komal Joshi <moudgillkomal6@gmail.com>
2024-03-25 21:05:47 +0530

    modified:   fast_exponentialtion.java

--- fast_exponentialtion.java
+++ fast_exponentialtion.java
@@ -5,22 +5,22 @@ class Main {
     // N value is given
     public static int power(int a, int n) {
         if(n == 0)
-//             return 1;
+            return 1;
         
-//         int partial = power(a, n/2);
+        int partial = power(a, n/2);
 
-//         if(n % 2 == 0)
-//             return partial * partial;
+        if(n % 2 == 0)
+            return partial * partial;
 
-//         return partial * partial * a;
-//     }
+        return partial * partial * a;
+    }
 
-//     public static void main(String[] args) {
-//         // Note: Don't change class name
-//         Scanner sc = new Scanner(System.in);
-//         int A = sc.nextInt();
-//         int N = sc.nextInt();
+    public static void main(String[] args) {
+        // Note: Don't change class name
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int N = sc.nextInt();
 
-//         System.out.println(power(A, N));
-//     }
-// }
+        System.out.println(power(A, N));
+    }
+}
