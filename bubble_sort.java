88fa8eefd748eaa2f941227ecdd1fe0949c1df27
Komal Joshi <moudgillkomal6@gmail.com>
2024-03-27 09:47:31 +0530

    modified:   bubble_sort.java

--- bubble_sort.java
+++ bubble_sort.java
@@ -24,19 +24,19 @@ class Main {
                 if(arr[i] > arr[i + 1])
                     swap(arr, i, i+1);
         }
-//     }
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-//         int N = sc.nextInt();
-//         int arr[] = new int[N];
+        int N = sc.nextInt();
+        int arr[] = new int[N];
 
-//         for(int i = 0; i < N; ++i)
-//             arr[i] = sc.nextInt();
+        for(int i = 0; i < N; ++i)
+            arr[i] = sc.nextInt();
 
-//          bubbleSort(arr);
+         bubbleSort(arr);
 
-//         for(int num : arr)
-//             System.out.print(num + " ");
-//     }
-// }
+        for(int num : arr)
+            System.out.print(num + " ");
+    }
+}
