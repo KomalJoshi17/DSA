111966c6b04767d6ed5ea779dae76db989ca7d74
Komal Joshi <moudgillkomal6@gmail.com>
2024-03-28 21:44:38 +0530

    modified:   deepcopy.java

--- deepcopy.java
+++ deepcopy.java
@@ -1,39 +1,39 @@
-// /*
-// // Definition for a ListNode.
-// class ListNode {
-//     int val;
-//     ListNode next;
-//     public ListNode(int val) {
-//         this.val = val;
-//         this.next = null;
-//     }
-// }
-// */
+/*
+// Definition for a ListNode.
+class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+*/
 
-// // No Link
+// No Link
 
-// class Solution {
+class Solution {
 
-//     public ListNode copyList(ListNode head) {
-//         if(head == null)
-//             return null;
+    public ListNode copyList(ListNode head) {
+        if(head == null)
+            return null;
 
-//         ListNode newHead = new ListNode(head.val);
+        ListNode newHead = new ListNode(head.val);
 
-//         ListNode cur = head;
-//         ListNode newCur = newHead;
+        ListNode cur = head;
+        ListNode newCur = newHead;
 
-//         while(cur.next != null) {
+        while(cur.next != null) {
 
-//             // Make a copy and do the attachment
-//             ListNode newNext = new ListNode(cur.next.val);
-//             newCur.next = newNext;
+            // Make a copy and do the attachment
+            ListNode newNext = new ListNode(cur.next.val);
+            newCur.next = newNext;
 
-//             // Move ahead
-//             cur = cur.next;
-//             newCur = newCur.next;
-//         }
+            // Move ahead
+            cur = cur.next;
+            newCur = newCur.next;
+        }
         
-//         return newHead;
-//     }
-// }
+        return newHead;
+    }
+}
