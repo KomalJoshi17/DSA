426f1d5468a1d9e8b0f8bf0076eb20d0cbe0855c
Komal Joshi <moudgillkomal6@gmail.com>
2024-03-29 21:34:28 +0530

    modified:   bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -19,17 +19,17 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
-// int main() {
-//     int n; cin >> n;
-//     int *arr = new int[n];
+int main() {
+    int n; cin >> n;
+    int *arr = new int[n];
 
-//     for(int i = 0; i < n; ++i)
-//         cin >> arr[i];
+    for(int i = 0; i < n; ++i)
+        cin >> arr[i];
 
-//     bubbleSort(arr, n);
+    bubbleSort(arr, n);
 
-//     for(int i = 0; i < n; ++i)
-//         cout << arr[i] << ' ';
+    for(int i = 0; i < n; ++i)
+        cout << arr[i] << ' ';
 
-// 	return 0;
-// }
+	return 0;
+}
