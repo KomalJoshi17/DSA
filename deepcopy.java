892319d289434f02061628a1b24a30d31cad3b1e
Komal Joshi <moudgillkomal6@gmail.com>
2024-03-29 21:32:43 +0530

    modified:   deepcopy.java

--- deepcopy.java
+++ deepcopy.java
@@ -1,14 +1,14 @@
-/*
-// Definition for a ListNode.
-class ListNode {
-    int val;
-    ListNode next;
-    public ListNode(int val) {
-        this.val = val;
-        this.next = null;
-    }
-}
-*/
+// /*
+// // Definition for a ListNode.
+// class ListNode {
+//     int val;
+//     ListNode next;
+//     public ListNode(int val) {
+//         this.val = val;
+//         this.next = null;
+//     }
+// }
+// */
 
 // No Link
 
