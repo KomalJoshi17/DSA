09eba696bfd00b2ecf7f8f191b54feb87c39c118
Komal Joshi <moudgillkomal6@gmail.com>
2024-04-05 23:16:57 +0530

    modified:   Different_Notation.cpp

--- Different_Notation.cpp
+++ Different_Notation.cpp
@@ -1,7 +1,7 @@
-// There are mainly three notations:
+There are mainly three notations:
 
-// linear and exponential function :- exponential will rise fast
-// Big Oh notation (O) - upper bound -> exponential 
-// Omega notation (Ω) - lower bound like class won't end before 2 hours it always goes beyond 2 hours
-// Theta notation (ፀ) - exact bound
+linear and exponential function :- exponential will rise fast
+Big Oh notation (O) - upper bound -> exponential 
+Omega notation (Ω) - lower bound like class won't end before 2 hours it always goes beyond 2 hours
+Theta notation (ፀ) - exact bound
 
